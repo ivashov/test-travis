110e0700604c35a03fc6cd04a256938c11046d11
Kirill Ivashov <ivashov@cs.petrsu.ru>
2015-04-01 15:52:57 +0300

    Add python test

--- script.sh
+++ script.sh
@@ -2,6 +2,8 @@
 
 export PATH="${HOME}/python/bin:${PATH}"
 
-/usr/bin/env python3 --version
-$HOME/python/bin/python3 --version
-exit 0
+/usr/bin/python3 --version
+
+php -S localhost:8000 -t scripts&
+sleep 0.1
+python3 test.py
---
+++ test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import urllib.request
+import unittest
+
+class TestTest(unittest.TestCase):
+    def test_test(self):
+        req = urllib.request.Request(url="http://localhost:8000/test.php")
+        with urllib.request.urlopen(req) as f:
+            response = f.read().decode('utf-8')
+            self.assertTrue(response, "Test");
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+
