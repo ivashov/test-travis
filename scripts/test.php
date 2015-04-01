ef658ae120599e3d876192a8ea139cc1e00dc761
Kirill Ivashov <ivashov@cs.petrsu.ru>
2015-04-01 16:11:29 +0300

    Fix

--- scripts/test.php
+++ scripts/test.php
@@ -3,7 +3,7 @@
 $dbconn = pg_connect("host=localhost dbname=tdatabase user=postgres");
 
 $res = pg_query($dbconn, "SELECT users.name FROM users;");
-$row = pg_fetch_row($res):
+$row = pg_fetch_row($res);
 
 if ($row) {
     echo $row[0];
